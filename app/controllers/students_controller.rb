5371ffc593b8eb3c35a0717936ff79d161782bcb
Eve Kozina <eve.kozina@gmail.com>
2022-01-10 15:20:50 -0500

    commit tree a932ad73cb5db488d512fe070f83f383d0f111fd

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+    def grades
+        students = Student.all.order(:grade).reverse_order
+        render json: students
+    end
+end
